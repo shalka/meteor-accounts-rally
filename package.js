6b47e40b59f6c3b866bea09f448fbaf32ae53344
Stanley Halka <shalka@rallydev.com>
2014-10-12 20:45:05 -0400

    update to rally-oauth services version 0.0.4 to make use of Meteor.settings

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Login service for Rally accounts.",
-  version: "0.0.3",
+  version: "0.0.4",
   git: "https://github.com/shalka/meteor-accounts-rally.git"
 });
 
@@ -8,10 +8,10 @@ Package.on_use(function(api) {
   api.versionsFrom("METEOR@0.9.0");
   api.use('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('shalka:rally-oauth@0.0.3', ['client', 'server']);
+  api.use('shalka:rally-oauth@0.0.4', ['client', 'server']);
 
   api.imply('accounts-base', ['client', 'server']);
-  api.use('shalka:rally-oauth@0.0.3', ['client', 'server']);
+  api.use('shalka:rally-oauth@0.0.4', ['client', 'server']);
 
   api.add_files(['rally_login_button.css'], 'client');
 
